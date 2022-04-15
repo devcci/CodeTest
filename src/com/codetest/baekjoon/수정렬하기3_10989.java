1592a59ea9422d0cf92d038e58829109554b64c2
devcci <jdj5250@gmail.com>
2022-04-15 22:17:18 +0900

    백준 풀이 10989

---
+++ src/com/codetest/baekjoon/수정렬하기3_10989.java
@@ -0,0 +1,29 @@
+package com.codetest.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class 수정렬하기3_10989 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        for (int i : arr) {
+            bw.write(i + "\n");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
